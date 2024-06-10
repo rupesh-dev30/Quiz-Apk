a7287f60937372e577c5664259d290bede0be71c
rupesh-dev30 <cgcrupesh@gmail.com>
2024-06-11 00:46:04 +0530

    useReducer v2

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -1,18 +1,29 @@
-import { useReducer, useState } from "react";
+import { useReducer } from "react";
 
 function reducer(state, action){
   console.log(state, action);
-  if(action.type === 'inc') return state + 1;
-  if(action.type === 'dec') return state - 1;
-  if(action.type === 'setCount') return action.payload;
+
+  switch(action.type){
+    case 'dec':
+      return {...state, count: state.count - state.step};
+    case 'inc':
+      return {...state, count: state.count + state.step};
+    case 'setCount':
+      return {...state, count: action.payload}; 
+    case 'setStep':
+      return {...state, step:action.payload};
+    case 'setReset':
+      return {...state, count: 0, step: 0};
+    default:
+      throw new Error("Unknown action");
+  }
 }
 
 function DateCounter() {
-  // const [count, setCount] = useState(0);
+  const initialState = {count: 0, step: 1};
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-  const [count, dispatch] = useReducer(reducer, 0);
-
-  const [step, setStep] = useState(1);
+  const {count, step} = state;
 
   // This mutates the date object.
   const date = new Date("june 21 2027");
@@ -20,28 +31,22 @@ function DateCounter() {
 
   const dec = function () {
     dispatch({type: "dec"});
-    // setCount((count) => count - 1);
-    // setCount((count) => count - step);
   };
 
   const inc = function () {
     dispatch({type: "inc"});
-    // setCount((count) => count + 1);
-    // setCount((count) => count + step);
   };
 
   const defineCount = function (e) {
     dispatch({type: "setCount", payload: Number(e.target.value)});
-    // setCount(Number(e.target.value));
   };
 
   const defineStep = function (e) {
-    setStep(Number(e.target.value));
+    dispatch({type: 'setStep', payload: Number(e.target.value)})
   };
 
   const reset = function () {
-    // setCount(0);
-    setStep(1);
+    dispatch({type: 'setReset'})
   };
 
   return (
