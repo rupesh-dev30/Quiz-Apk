8fb1097aee763540d822718601a99ab1b1e53b2b
rupesh-dev30 <cgcrupesh@gmail.com>
2024-06-11 01:37:50 +0530

    quiz apk initital

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
-import DateCounter from "./DateCounter"
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+
+const initialState = {
+  questions: [],
+
+  //"loading" or "error" or "ready" or "active" or "finished" state
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecieved":
+      return {
+        ...state,
+        question: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Action Unknown!");
+  }
+}
 
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataRecieved", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1-15</p>
+        <p>Questions?</p>
+      </Main>
     </div>
   );
 }
-
-
