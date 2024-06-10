c8cf12b3f01a283adaba8778c00bd7f7b554d929
rupesh-dev30 <cgcrupesh@gmail.com>
2024-06-11 00:32:44 +0530

    useReducer

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -2,7 +2,9 @@ import { useReducer, useState } from "react";
 
 function reducer(state, action){
   console.log(state, action);
-  return state + action;
+  if(action.type === 'inc') return state + 1;
+  if(action.type === 'dec') return state - 1;
+  if(action.type === 'setCount') return action.payload;
 }
 
 function DateCounter() {
@@ -17,18 +19,19 @@ function DateCounter() {
   date.setDate(date.getDate() + count);
 
   const dec = function () {
-    dispatch(-1);
+    dispatch({type: "dec"});
     // setCount((count) => count - 1);
     // setCount((count) => count - step);
   };
 
   const inc = function () {
-    dispatch(1);
+    dispatch({type: "inc"});
     // setCount((count) => count + 1);
     // setCount((count) => count + step);
   };
 
   const defineCount = function (e) {
+    dispatch({type: "setCount", payload: Number(e.target.value)});
     // setCount(Number(e.target.value));
   };
 
