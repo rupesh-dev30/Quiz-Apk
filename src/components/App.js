0243681845d755267be388017e5025240ced7150
rupesh-dev30 <cgcrupesh@gmail.com>
2024-06-12 13:08:47 +0530

    Finished Quiz Apk

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,9 @@ import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
 import Question from "./Question";
+import NextButton from "./NextButton";
+import Progress from "./Progress";
+import FinishScreen from "./FinishScreen";
 
 const initialState = {
   questions: [],
@@ -12,6 +15,9 @@ const initialState = {
   //"loading" or "error" or "ready" or "active" or "finished" state
   status: "loading",
   index: 0,
+  anwer: null,
+  points: 0,
+  highscore: 0,
 };
 
 function reducer(state, action) {
@@ -29,6 +35,37 @@ function reducer(state, action) {
       };
     case "start":
       return { ...state, status: "active" };
+    case "newAnswer":
+      const question = state.questions.at(state.index);
+
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+    case "nextQuestion":
+      return { ...state, index: state.index + 1, answer: null };
+    case "finished":
+      return {
+        ...state,
+        status: "finished",
+        highscore:
+          state.points > state.highscore ? state.points : state.highscore,
+      };
+    case "restart":
+      return { ...initialState, questions: state.questions, status: "ready" };
+
+      // return {
+      //   ...state,
+      //   points: 0,
+      //   highscore: 0,
+      //   index: 0,
+      //   answer: null,
+      //   status: "ready",
+      // };
 
     default:
       throw new Error("Action Unknown!");
@@ -37,12 +74,14 @@ function reducer(state, action) {
 
 export default function App() {
   // const [state, dispatch] = useReducer(reducer, initialState);
-  const [{ questions, status, index }, dispatch] = useReducer(
-    reducer,
-    initialState
-  );
+  const [{ questions, status, index, answer, points, highscore }, dispatch] =
+    useReducer(reducer, initialState);
 
   const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce(
+    (prev, curr) => prev + curr.points,
+    0
+  );
 
   useEffect(function () {
     fetch("http://localhost:8000/questions")
@@ -60,7 +99,37 @@ export default function App() {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question question={questions[index]} />}
+        {status === "active" && (
+          <>
+            <Progress
+              index={index}
+              numQuestion={numQuestions}
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+              answer={answer}
+            />
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <NextButton
+              dispatch={dispatch}
+              answer={answer}
+              index={index}
+              numQuestions={numQuestions}
+            />
+          </>
+        )}
+
+        {status === "finished" && (
+          <FinishScreen
+            points={points}
+            maxPossiblePoints={maxPossiblePoints}
+            highscore={highscore}
+            dispatch={dispatch}
+          />
+        )}
       </Main>
     </div>
   );
--- src/components/Options.js
+++ src/components/Options.js
@@ -1,9 +1,24 @@
-export default function Options({question}) {
+export default function Options({ question, dispatch, answer }) {
+  const hasAnswer = answer != null;
+
   return (
     <div className="options">
-        {question.options.map((option) => (
-          <button className="btn btn-option" key={option}>{option}</button>
-        ))}
-      </div>
-  )
+      {question.options.map((option, index) => (
+        <button
+          className={`btn btn-option ${index === answer ? "answer" : ""} ${
+            hasAnswer
+              ? index === question.correctOption
+                ? "correct"
+                : "wrong"
+              : ""
+          }`}
+          key={option}
+          disabled={hasAnswer}
+          onClick={() => dispatch({ type: "newAnswer", payload: index })}
+        >
+          {option}{" "}
+        </button>
+      ))}
+    </div>
+  );
 }
